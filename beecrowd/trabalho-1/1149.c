2135c32130fb5387a291d14aeff5b9aa43796ffb
Augustodalpra12 <100322707+Augustodalpra12@users.noreply.github.com>
2022-05-26 19:28:25 -0300

    Create 1149.c

---
+++ beecrowd/trabalho-1/1149.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, n, soma = 0;
+    
+    scanf("%d %d", &a, &n);
+    
+    while(n <= 0)
+    scanf("%d", &n);
+    
+    for(int i = 0; i <= n - 1; i++)
+    {
+        soma += a + i;
+        
+    }
+    printf("%d\n", soma);
+    
+    return 0;
+}
