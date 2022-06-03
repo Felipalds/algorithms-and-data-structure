1eb057c8f1f93113bde78b7b8a6a824998d02c3b
Luiz Felipe Fonseca Rosa <luizfelipefonsecarosa@gmail.com>
2022-06-03 11:30:38 -0300

    finishing trees

--- trees/Tree.c
+++ trees/Tree.c
@@ -56,12 +56,163 @@ TreeNode* BTinsert(Tdata x, TreeNode *bt)
         }
 }
 
+// ===================
+//  Remoção da árvore
+// ===================
+TreeNode *BTdelete(Tdata x, TreeNode *bt)
+{
+    TreeNode *p, *p2;
+    if(bt->info == x)
+    {
+        if(bt->left == bt->right)
+        {
+            free(bt);
+            return NULL;
+        }
+        else 
+        {
+            if(bt->left == NULL)
+            {
+                p = bt->right;
+                bt = NULL;
+            }
+        }
+    }
+}
+
+TreeNode* BTsearch(Tdata x, TreeNode *bt)
+{
+    if(bt == NULL)
+        return 0;
+    else if (bt->info == x)
+        return bt;
+    else if(x < bt->info)
+        return BTsearch(x, bt->left);
+    else
+        return BTsearch(x, bt->right);
+}
+
+void BreadthFirst(TreeNode *bt)
+{
+    TreeNode *raiz = bt;
+    TListSE fila;
+
+    initList(&fila);
+    if(raiz != NULL)
+    {
+        insertRight(raiz, &fila); // insere a raiz na fila
+        while(!emptyList(fila))
+        {
+            raiz = head(fila); //pega o elemento na cabeça da fila
+            removeFirst(&fila);
+            printf("%hd", raiz->info); //imprime
+            if(raiz->left != NULL)
+                insertRight(raiz->left, &fila);
+            if(raiz->right != NULL)
+                insertRight(raiz->right, &fila);
+        }
+    }
+}
+
+void PreOrder(TreeNode *bt)
+{
+    if(bt == NULL)
+        return;
+    else
+    {
+        printf("%hd ", bt->info);
+        PreOrder(bt->left);
+        PreOrder(bt->right);
+    }
+}
+
+void InOrder(TreeNode *bt)
+{
+    if(bt == NULL)
+        return;
+    else
+    {
+        InOrder(bt->left);
+        printf("%hd ", bt->info);
+        InOrder(bt->right);
+    }
+}
+
+void PostOrder(TreeNode *bt)
+{
+    if(bt == NULL)
+        return;
+    else
+    {
+        PostOrder(bt->left);
+        PostOrder(bt->right);
+        printf("%hd ", bt->info);
+
+    }
+}
+
+short HeightTree(TreeNode *bt)
+{
+    if(bt == NULL)
+        return 0;
+    else
+    {
+        short valueLeft = HeightTree(bt->left);
+        short valueRight = HeightTree(bt->right);
+        if(valueLeft > valueRight)
+            return 1 + valueLeft;
+        else
+            return 1 + valueRight;
+    }
+}
+
+short LengthTree(TreeNode *bt)
+{
+    if(bt == NULL)
+        return 0;
+    else
+    {
+        if(bt->left == bt->right) return 1;
+        else return (LengthTree(bt->left) + LengthTree(bt->right));
+    }
+}
+
+short NodeHeight(TreeNode *bt, short value)
+{
+    if(bt == NULL)
+        return 0;
+    else if (bt->info == value)
+        return 1;
+    else if(value < bt->info)
+        return 1 + NodeHeight(bt->left, value);
+    else
+        return 1 + NodeHeight(bt->right, value);
+}
+
+short NodeDistance(TreeNode *bt, short x, short y)
+{
+    if(bt == NULL)
+        return 0;
+    else if(bt->info > x && bt->info > y)
+        NodeDistance(bt->left, x, y);
+    else if(bt->info < x && bt->info < y)
+        NodeDistance(bt->right, x, y);
+    else
+    {
+        short h1 = NodeHeight(bt, x) - 1;
+        short h2 = NodeHeight(bt, y) - 1;
+        return h1 + h2;
+    }
+}
+
 // \t é uma tabulação, um TAB
 int main(){
     TreeNode *BT = NULL; //ponteiro pára a raiz para a arvore
     int op;
     Tdata v;
     TreeNode *found = NULL;
+
+
     do
     {
         printf("Árvores Binárias de Busca (BST)\n");
@@ -77,6 +228,12 @@ int main(){
         printf("\t(5)Percurso pré-ordem\n");
         printf("\t(6)Percurso em ordem\n");
         printf("\t(7)Percurso pós-ordem\n");
+        printf("\t(8)Altura da árvore\n");
+        printf("\t(9)Largura da árvore\n");
+        printf("\t(10)Altura do nó\n");
+        printf("\t(11)Distâncias entre nós\n");
+
+
         scanf("%d", &op);
 
         switch(op){
@@ -84,19 +241,68 @@ int main(){
                 printf("Saindo. . .");
                 break;
             case 1:
+                printf("Qual valor? >>");
+                scanf("%hu", &v);
+                BT = BTinsert(v, BT);
+                break;
             case 2:
+                printf("Qual valor? >>");
+                scanf("%hu", &v);
+                BT = BTdelete(v, BT);
+                break;
             case 3:
+                printf("Qual valor? >>");
+                scanf("%hu", &v);
+                found = BTsearch(v, BT);
+                if(found)
+                    printf(">>> Valor encontrado no endereço %p <<<\n", found);
+                else 
+                    printf(">>> Valor não encontrado <<< \n");
+                break;
             case 4:
-            case 5:
-            case 6:
-
-            case 7:
-                printf("\t\t >> Percurso em pós-ordem <<\n");
+                printf("\t\t>> Percurso em largura <<\n");
+                BreadthFirst(BT);
                 printf("\n\n");
                 break;
+            case 5:
+                printf("\t\t>> Percurso em pré-ordem <<\n");
+                PreOrder(BT);
+                printf("\n\n");
+                break;
+            case 6:
+                printf("\t\t>> Percurso em ordem <<\n");
+                InOrder(BT);
+                printf("\n\n");
+                break;
+            case 7:
+                printf("\t\t >> Percurso em pós-ordem <<\n");
+                PostOrder(BT);
+                printf("\n\n");
+                break;
+            case 8:
+                printf("\t\t >> Altura da árvore << \n");
+                printf("%hd", HeightTree(BT));
+                printf("\n\n");
+                break;
+            case 9:
+                printf("\t\t >> Largura da árvore << \n");
+                printf("%hd", LengthTree(BT));
+                printf("\n\n");
+                break;
+            case 10:
+                printf("\t\t >> Altura do nó << \n");
+                short value;
+                scanf("%hd", &value);
+                printf(" > > > > Altura é: %hd\n\n", NodeHeight(BT, value));
+            case 11:
+                printf("\t\t >> Distância entre dois nós << \n");
+                short value1, value2;
+                scanf("%hd %hd", &value1, &value2);
+                printf(" > > > Distância é: %hd \n \n", NodeDistance(BT, value1, value2));
         }
 
     }while(op != 0);
+    
     printf("Limpando a árvore . . .\n");
     //BTclear(BT);
     printf("Árvore limpa! \n");
